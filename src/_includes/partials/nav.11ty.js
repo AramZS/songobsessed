5fc7ff6d61ddcbc91d0c7b4cb0d7fce1fae3b630
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2023-12-22 16:33:54 -0500

    Get navigation working

--- src/_includes/partials/nav.11ty.js
+++ src/_includes/partials/nav.11ty.js
@@ -2,12 +2,12 @@ const linkmaker = require("../../utils/linkmaker");
 
 module.exports = function (data) {
 	return /*html*/ `
+<script>
+function opennav(e){
+	console.log(e.parentNode.parentNode); e.parentNode.parentNode.classList.toggle('open');
+}
+</script>	
 <nav id="top-nav">
-	<script>
-	function opennav(e){
-		console.log(e.parentNode.parentNode); e.parentNode.parentNode.classList.toggle('open');
-	}
-	</script>
 	<div id="nav-icon" onclick="opennav(this)">
 		<img src="/assets/menu-icon.svg" />
 	</div>
