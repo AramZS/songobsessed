3fb614d71e3400ea5f0f85eec782cac01da0a3cb
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2023-12-27 00:20:48 -0500

    Put together an autoplay toggle and give it some basic styling

--- src/_includes/partials/nav.11ty.js
+++ src/_includes/partials/nav.11ty.js
@@ -12,18 +12,18 @@ function opennav(e){
 		<img src="/assets/menu-icon.svg" />
 	</div>
 	<h1>${linkmaker(data, "", data.site.title)}</h1>
-	<div id="nav-menu-items"><ul>
-		<li>${linkmaker(data, "/songs/", "Songs")}</li>
-		<li>${linkmaker(data, "/search/", "Search")}</li>
-		<li>${linkmaker(data, "/how-to-scrobble/", "Scrobble This Site")}</li>
-		<li>${linkmaker(data, "/about/", "What is This?")}</li>
-		<li>
-			<label class="switch">
-				<input type="checkbox" id="xplayer-autoplay-switch" checked>
-				<span class="slider round"></span>
-	  		</label>
-		</li>
-	</ul></div>
+		<div id="nav-menu-items"><ul>
+			<li>${linkmaker(data, "/songs/", "Songs")}</li>
+			<li>${linkmaker(data, "/search/", "Search")}</li>
+			<li>${linkmaker(data, "/how-to-scrobble/", "Scrobble This Site")}</li>
+			<li>${linkmaker(data, "/about/", "What is This?")}</li>
+		</ul>
+	</div>
+	<label class="switch">
+		<span class="switch-text">Autoplay</span>
+		<input type="checkbox" id="xplayer-autoplay-switch" checked>
+		<span class="slider round"></span>
+  </label>
 </nav>
 `;
 };
