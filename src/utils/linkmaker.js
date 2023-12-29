5a32a3125049eb053fbe684e819059aae6022b68
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2023-12-29 16:24:29 -0500

    touch ups

--- src/utils/linkmaker.js
+++ src/utils/linkmaker.js
@@ -1,5 +1,6 @@
 module.exports = function (data, localLink, content, classNames) {
 	let link = `${data.site.domain}${localLink}`; // hx-get="${link}"
+	classNames = classNames ? classNames : "htmx-made-link";
 	return /*html*/ `
 	<a href="${link}" hx-boost="true" hx-swap="outerHTML show:top" hx-target="#main-content" hx-push-url="true" hx-select="#main-content" class="${classNames}">${content}</a>
 	`;
