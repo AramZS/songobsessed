2cf9ad14cf8ab9a16645b34a792973ee296353a3
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2024-03-04 18:55:29 -0500

    Set autoplay to off for firsttime landers on a song page

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,3 +1,18 @@
 if ("serviceWorker" in navigator) {
 	navigator.serviceWorker.register("/service-worker.js");
 }
+
+let pageCount = sessionStorage.getItem("pageCount");
+if (pageCount === null) {
+	pageCount = 0;
+}
+
+if (
+	(null === localStorage.getItem("xplayerRetainedSettings") ||
+		pageCount === 0) &&
+	window["xplayer-setup"]
+) {
+	window["xplayer-autoplay-switch"].checked = false;
+}
+
+++pageCount;
